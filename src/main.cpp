2ac7442797d31d766ffdc8d49497296ea467b227
Jon Rafkind <jon@rafkind.com>
2017-11-21 13:34:07 -0800

    make a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,33 @@
-#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
+    Display * display;
+    Window window;
+
+    display = XOpenDisplay(NULL);
+    if (display == NULL){
+        std::cerr << "Cannot open display" << std::endl;
+        return 1;
+    }
+
+    int screen = DefaultScreen(display);
+    window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 100, 100, 1, BlackPixel(display, screen), WhitePixel(display, screen));
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+    XMapWindow(display, window);
+
+    while (1){
+        XEvent event;
+        XNextEvent(display, &event);
+        if (event.type == Expose){
+            XFillRectangle(display, window, DefaultGC(display, screen), 20, 20, 10, 10);
+            XDrawString(display, window, DefaultGC(display, screen), 10, 50, "hi", strlen("hi"));
+        } else if (event.type == KeyPress){
+            break;
+        }
+    }
+
+    XCloseDisplay(display);
 }
