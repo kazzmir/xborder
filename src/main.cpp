1b08f57b4eb112f4788a3340bbca26473359a371
Jon Rafkind <jon@rafkind.com>
2017-11-21 14:33:43 -0800

    reparent child window, set the background color to yellow

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,43 @@ int main(){
     }
 
     int screen = DefaultScreen(display);
-    window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 100, 100, 1, BlackPixel(display, screen), WhitePixel(display, screen));
-    XSelectInput(display, window, ExposureMask | KeyPressMask);
+
+    int use_terminal = 0x38d34c0;
+
+    XColor yellow;
+    memset(&yellow, 0, sizeof(XColor));
+    yellow.flags = DoRed | DoGreen | DoBlue;
+    yellow.red = 65535;
+    yellow.green = 65535;
+    yellow.blue = 0;
+    Colormap screen_colormap = DefaultColormap(display, screen);
+    int yellow_color = XAllocColor(display, screen_colormap, &yellow);
+    std::cout << "yellow color " << yellow_color << std::endl;
+
+    XWindowAttributes child_attributes;
+    XGetWindowAttributes(display, use_terminal, &child_attributes);
+
+    int border_size = 3;
+
+    window = XCreateSimpleWindow(display, RootWindow(display, screen), child_attributes.x, child_attributes.y, child_attributes.width + border_size * 2, child_attributes.height + border_size * 2, 1, BlackPixel(display, screen), yellow.pixel);
+    XSelectInput(display, window, ExposureMask | KeyPressMask | StructureNotifyMask);
     XMapWindow(display, window);
+
+    XReparentWindow(display, use_terminal, window, border_size, border_size);
 
     while (1){
         XEvent event;
         XNextEvent(display, &event);
         if (event.type == Expose){
+            /*
             XFillRectangle(display, window, DefaultGC(display, screen), 20, 20, 10, 10);
             XDrawString(display, window, DefaultGC(display, screen), 10, 50, "hi", strlen("hi"));
+            */
         } else if (event.type == KeyPress){
             break;
         }
     }
 
+    XReparentWindow(display, use_terminal, RootWindow(display, screen), child_attributes.x, child_attributes.y);
     XCloseDisplay(display);
 }
